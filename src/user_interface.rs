e3c20c4c9644f1775418cda814fc2583f3aa0589
Philip Kaiser <pk30.05@hotmail.de>
2022-07-18 09:01:44 +0200

    Add combat log stub

--- src/user_interface.rs
+++ src/user_interface.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{ecs::system::EntityCommands, prelude::*};
 
 use crate::{
     components::CombatStats::CombatStats,
@@ -11,11 +11,13 @@ pub struct UIPlugin {}
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(setup).add_system_set(
-            SystemSet::on_update(GameState::Render)
-                .with_system(render_ui)
-                .after(RENDER_MAP_LABEL),
-        );
+        app.add_startup_system(setup)
+            .add_system_set(
+                SystemSet::on_update(GameState::Render)
+                    .with_system(render_ui)
+                    .after(RENDER_MAP_LABEL),
+            )
+            .add_system_set(SystemSet::on_enter(GameState::AwaitingInput).with_system(render_ui));
     }
 }
 
@@ -25,6 +27,9 @@ pub struct HealthBar {}
 #[derive(Component)]
 pub struct HealthText {}
 
+#[derive(Component)]
+pub struct CombatLog {}
+
 pub struct UIFont(Handle<Font>);
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
@@ -32,59 +37,101 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let font_handle: Handle<Font> = asset_server.load("fonts\\EduVICWANTBeginner-Regular.ttf");
     commands.insert_resource(UIFont(font_handle.clone()));
 
-    // root node
-    commands
-        .spawn_bundle(NodeBundle {
-            style: Style {
-                size: Size::new(Val::Percent(100.0), Val::Percent(20.0)),
-                justify_content: JustifyContent::SpaceBetween,
-                ..default()
-            },
-            color: Color::PURPLE.into(),
+    let mut commands_builder = commands.spawn_bundle(NodeBundle {
+        style: Style {
+            justify_content: JustifyContent::SpaceBetween,
+            size: Size::new(Val::Percent(100.0), Val::Percent(20.0)),
             ..default()
-        })
-        .with_children(|parent| {
-            parent
-                .spawn_bundle(NodeBundle {
-                    style: Style {
-                        size: Size::new(
-                            Val::Px(PLAYER_STARTING_HEALTH as f32 * 3.0),
-                            Val::Percent(10.0),
-                        ),
-                        position_type: PositionType::Absolute,
-                        position: Rect {
-                            left: Val::Percent(50.0),
-                            top: Val::Percent(25.0),
-                            ..default()
-                        },
-                        justify_content: JustifyContent::SpaceBetween,
+        },
+        color: Color::PURPLE.into(),
+        ..default()
+    });
+    spawn_health_bar(&mut commands_builder, font_handle.clone());
+    spawn_combat_log(&mut commands_builder, font_handle);
+}
+
+pub fn spawn_health_bar(commands: &mut EntityCommands, text_font: Handle<Font>) {
+    commands.with_children(|parent| {
+        parent
+            .spawn_bundle(NodeBundle {
+                style: Style {
+                    size: Size::new(
+                        Val::Px(PLAYER_STARTING_HEALTH as f32 * 3.0),
+                        Val::Percent(10.0),
+                    ),
+                    position_type: PositionType::Absolute,
+                    position: Rect {
+                        left: Val::Percent(50.0),
+                        top: Val::Percent(25.0),
                         ..default()
                     },
-                    color: Color::RED.into(),
+                    justify_content: JustifyContent::SpaceBetween,
                     ..default()
-                })
-                .insert(HealthBar {})
-                .with_children(|parent| {
-                    parent
-                        .spawn_bundle(TextBundle {
-                            style: Style {
-                                margin: Rect::all(Val::Px(5.0)),
-                                ..default()
-                            },
-                            text: Text::with_section(
-                                format!("{}/{}", PLAYER_STARTING_HEALTH, PLAYER_STARTING_HEALTH),
-                                TextStyle {
-                                    font: font_handle,
-                                    font_size: 27.0,
-                                    color: Color::WHITE,
-                                },
-                                Default::default(),
-                            ),
+                },
+                color: Color::RED.into(),
+                ..default()
+            })
+            .insert(HealthBar {})
+            .with_children(|parent| {
+                parent
+                    .spawn_bundle(TextBundle {
+                        style: Style {
+                            margin: Rect::all(Val::Px(5.0)),
                             ..default()
-                        })
-                        .insert(HealthText {});
+                        },
+                        text: Text::with_section(
+                            format!("{}/{}", PLAYER_STARTING_HEALTH, PLAYER_STARTING_HEALTH),
+                            TextStyle {
+                                font: text_font,
+                                font_size: 27.0,
+                                color: Color::WHITE,
+                            },
+                            Default::default(),
+                        ),
+                        ..default()
+                    })
+                    .insert(HealthText {});
+            });
+    });
+}
+
+pub fn spawn_combat_log(commands: &mut EntityCommands, text_font: Handle<Font>) {
+    commands.with_children(|parent| {
+        parent
+            .spawn_bundle(NodeBundle {
+                style: Style {
+                    flex_direction: FlexDirection::ColumnReverse,
+                    size: Size::new(Val::Percent(40.0), Val::Percent(100.0)),
+                    ..default()
+                },
+                color: Color::rgb(0.8, 0.8, 1.0).into(),
+                ..default()
+            })
+            .with_children(|parent| {
+                parent.spawn_bundle(TextBundle {
+                    style: Style {
+                        margin: Rect {
+                            left: Val::Percent(1.0),
+                            right: Val::Percent(1.0),
+                            bottom: Val::Percent(1.0),
+                            ..default()
+                        },
+                        ..default()
+                    },
+                    text: Text::with_section(
+                        "Adventure awaits!",
+                        TextStyle {
+                            font: text_font,
+                            font_size: 27.0,
+                            color: Color::WHITE,
+                        },
+                        Default::default(),
+                    ),
+                    ..default()
                 });
-        });
+            })
+            .insert(CombatLog {});
+    });
 }
 
 pub fn render_ui(
